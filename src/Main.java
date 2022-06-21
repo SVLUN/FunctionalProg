4518ace6f9810f8206e930a9ca9a717e92c83850
SVLUN <107566052+SVLUN@users.noreply.github.com>
2022-06-21 14:05:33 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Funtional P");
-        System.out.println("Funtional P 2");
+        System.out.println("Functional P");
+        System.out.println("Functional P 2");
     }
 }
